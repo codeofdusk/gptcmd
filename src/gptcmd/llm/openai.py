3badc0785a12bf554534e8d3228292f68a850643
Bill Dengler <codeofdusk@gmail.com>
2024-11-20 14:37:11 -0800

    Enable streaming on o1 since it's now supported

--- src/gptcmd/llm/openai.py
+++ src/gptcmd/llm/openai.py
@@ -143,9 +143,6 @@ class OpenAI(LLMProvider):
             + Decimal(sampled_tokens) * sampled_scale
         ) * Decimal("100")
 
-    def _supports_streaming(self) -> bool:
-        return not self.model.startswith("o1")
-
     def complete(self, messages: Sequence[Message]) -> LLMResponse:
         kwargs = {
             "model": self.model,
@@ -222,16 +219,11 @@ class OpenAI(LLMProvider):
 
     @property
     def stream(self) -> bool:
-        return self._supports_streaming() and self._stream
+        return self._stream
 
     @stream.setter
     def stream(self, val: bool):
-        if not self._supports_streaming():
-            raise NotImplementedError(
-                "Streamed responses are not supported by this model"
-            )
-        else:
-            self._stream = val
+        self._stream = val
 
     @property
     def valid_models(self) -> Iterable[str]:
